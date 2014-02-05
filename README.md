d598681f3461f7a7bb2d2cb224a9710c4c60b184
Steve Comrie <steve@simplicate.ca>
2014-02-05 02:54:22 -0500

    Added migrate.sh script

--- README.md
+++ README.md
@@ -2,3 +2,23 @@ mysql-version-control
 =====================
 
 A Simple bash shell script to keep a series of migration logs of all schema changes to a MySQL database
+
+This script requires the Perl based mysqldiff tool in order to work properly.
+
+Usage
+-----
+
+Place the script in a directory off your project root (e.g. mysql_version) and configure the database access variables at the top of the script.
+
+Have your build tool (or manually) call the script before each commit.
+
+The script will create two sub-directories.
+
+./logs which will hold a history of all mysql schema dumps
+
+./migrations which will hold a numbered series of schema updates required to move the database through each set of changes.
+
+Running the script when no changes have been made to the database scheme will result in no log or migration files being generated.
+
+
+
---
+++ migrate.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# configure
+USER="user"
+PASS="password"
+DB="database_name"
+
+# change to where the script is stored
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+echo $DIR
+cd $DIR
+
+# make log directories
+mkdir -p ./migrations
+mkdir -p ./logs
+
+# dump the mysql schema
+mysqldump -d -u $USER --password=$PASS --opt $DB --single-transaction --add-drop-table=FALSE | sed 's/ AUTO_INCREMENT=[0-9]*\b//' > ./logs/dbdump-$(date +%Y-%m-%d-%T).sql
+
+# create a diff
+NEWDIFF="$(find ./migrations -type f | wc -l)"
+if [[ $(find ./logs -type f | wc -l) -gt 1 ]]; then    
+    mysqldiff $(ls ./logs/dbdump*|awk '{print $1}'|tail -2 | tr "\\n" " ") > ./migrations/${NEWDIFF}.sql
+else
+    cp $(ls ./logs/dbdump*|awk '{print $1}'|tail -1) ./migrations/0.sql
+fi
+
+# don't bother saving empty files & schema logs that haven't changed
+if [ ! -s ./migrations/${NEWDIFF}.sql ]; then
+    unlink ./migrations/${NEWDIFF}.sql
+    unlink $(ls ./logs/dbdump*|awk '{print $1}'|tail -1)
+fi
